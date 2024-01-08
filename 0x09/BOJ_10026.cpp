ef0bdb4e9d3d2f8ffaf1e25c06ef578f1afb4402
re-ag <763k357@naver.com>
2024-01-08 19:19:32 +0900

    Update BOJ_10026.cpp
    답변 참고하여 코드 정리

--- 0x09/BOJ_10026.cpp
+++ 0x09/BOJ_10026.cpp
@@ -8,63 +8,58 @@ using namespace std;
 #define Y second
 
 string board[101];
-string blind[101];
-int dist[101][101];
+bool dist[101][101];
 int n;
 
 int dx[4] = { -1,1,0,0 };
 int dy[4] = { 0,0,-1,1 };
 
 
-int bfs(string board[101], int N) {
-    map<char, int> m;
+void bfs(int i, int j) {
     queue<pair<int, int>> q;
-    for (int i = 0; i < n; i++)
-        fill(dist[i], dist[i] + n, -1);
+    q.push({ i,j });
+    dist[i][j] = true;
+    while (!q.empty()) {
+        auto cur = q.front(); q.pop();
+        for (int dir = 0; dir < 4; ++dir) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+            if (dist[nx][ny] || board[nx][ny] != board[i][j]) continue;
+            q.push({ nx,ny });
+            dist[nx][ny] = true;
+        }
+    }
+}
+int area() {
+    int cnt = 0;
+    queue<pair<int, int>> q;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (dist[i][j] < 0) {
-                char curChar = board[i][j];
-                if (m.find(curChar) == m.end())
-                    m[curChar] = 1;
-                else {
-                    ++m[curChar];
-                }
-                dist[i][j] = m[curChar];
-                q.push({ i,j });
-                while (!q.empty()) {
-                    auto cur = q.front(); q.pop();
-                    for (int dir = 0; dir < 4; ++dir) {
-                        int nx = cur.X + dx[dir];
-                        int ny = cur.Y + dy[dir];
-                        if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-                        if (dist[nx][ny] > 0 || board[nx][ny] != curChar) continue;
-                        q.push({ nx,ny });
-                        dist[nx][ny] = m[curChar];
-                    }
-                }
+            if (!dist[i][j]) {
+                ++cnt;
+                bfs(i, j);
             }
         }
     }
-    int sum = 0;
-    for (auto x : m)
-        sum += x.second;
-    return sum;
+    return cnt;
 }
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> board[i];
-        blind[i] = board[i];
     }
 
+    cout << area() << ' ';
+
+    for (int i = 0; i < n; i++)
+        fill(dist[i], dist[i] + n, false);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (blind[i][j] == 'R')
-                blind[i][j] = 'G';
+            if (board[i][j] == 'R')
+                board[i][j] = 'G';
         }
     }
-
-    cout << bfs(board, n) << " " << bfs(blind, n);
+    cout << area();
 }
