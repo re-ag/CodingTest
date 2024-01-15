beac4a854414a816b899ac7036bbf693eccb5e3e
re-ag <763k357@naver.com>
2024-01-15 15:39:14 +0900

    [BOJ] 텀 프로젝트/골드 3/??분
    * 시간 초과
    * 순환 그래프에 속하냐 속하지 않느냐가 일단 첫번째 포인트

--- 0x09/BOJ_9466.cpp
+++ 0x09/BOJ_9466.cpp
@@ -4,55 +4,42 @@ using namespace std;
 	BOJ 9466 텀 프로젝트
 	https://www.acmicpc.net/problem/9466
 
-	stud[n] 이 나 자신일 때 까지 순회
+	arr[n] 이 나 자신일 때 까지 순회
 	-> 순회하다가 나 자신을 발견하면 하나의 팀이 됨
 	-> 순회하다가 자기 자신을 선택한 사람을 발견하면, 난 팀이 될 수 없음
+
+
+	결국 팀이 되지 못하는 사람은 -> 순환 그래프 내 속하지 않는 노드임
+	따라서 모든 arr원소에 대해서 순환 그래프에 속하는지 아닌지 검사하여 판단 가능
+	But, O(N^2)
 */
 
 int test, n;
-int stud[100001];
-int team[100001];
-bool vis[100001];
+int arr[100001];
+int state[100001];
+
+bool iscycle(int idx) {
+	int cur = idx;
+	for (int i = 1; i <= n; i++) {
+		cur = arr[cur];
+		if (cur == idx) return true;
+	}
+	return false;
+}
 int main() {
 	queue<int> q;
 	cin >> test;
 	while (test--) {
+		int cnt = 0;
 		cin >> n;
 		for (int i = 1; i <= n; i++) {
-			cin >> stud[i];
-			team[i] = 0;
+			cin >> arr[i];
+			state[i] = 0;
 		}
-		// 3 1 3 7 3 4 6
 		for (int i = 1; i <= n; i++) {
-			if (team[i] == 0) {
-				int isTeam = -1;
-				q.push(i);
-				vis[i] = true;
-				while (!q.empty()) {
-					int me = q.front();
-					int nxt = stud[me]; q.pop();
-					if (team[nxt] == -1) {
-						break;
-					}
-					if (nxt == stud[nxt]) {
-						team[nxt] = 1;
-						if (nxt == i) isTeam = 1;
-					}
-					else if (nxt == i) {
-						isTeam = 1;
-					}
-					else {
-						q.push(nxt);
-						vis[nxt] = true;
-					}
-				}
-				team[i] = isTeam;
-
-			}
+			if (!iscycle(i)) cnt++;
 		}
-		int cnt = 0;
-		for (int i = 1; i <= n; i++)
-			if (team[i] == -1) cnt++;
+
 		cout << cnt << '\n';
 	}
 }
