3ee851fa001e94cbeca5d1d8443dac45d6068e7b
Jaeeun Kim <763k357@naver.com>
2024-01-17 19:47:56 +0900

    [BOJ] 말이 되고픈 원숭이 / 골드 3 / +30분
    문제를 제대로 읽자....^______________^

--- 0x09/BOJ_1600.cpp
+++ 0x09/BOJ_1600.cpp
@@ -26,14 +26,14 @@ int hdy[] = { -2, -1, 1, 2, -2, -1, 1, 2 };
 int arr[201][201];
 int dist[201][201][31];
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	queue<tuple<int, int, int>> q;
 	cin >> K >> w >> h;
-	for (int i = 0; i < w; i++) {
-		for (int j = 0; j < h; j++) {
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
 			cin >> arr[i][j];
-			//fill(dist[i][j], dist[i][j] + K, -1);
-			for (int k = 0; k <= K; k++)
-				dist[i][j][k] = -1;
+            fill(dist[i][j], dist[i][j] + K +1, -1);
 		}
 	}
 	q.push({ 0,0,0 });
@@ -42,17 +42,16 @@ int main() {
 		int x, y, k;
 		tie(x, y, k) = q.front(); q.pop();
 		int d = dist[x][y][k];
-		cout << "(" << x << ", " << y << ") k = " << k << '\n';
 		// k가 K보다 작다면 말처럼 움직이기
 		if(k < K){
 			for (int dir = 0; dir < 8; dir++) {
 				int nx = hdx[dir] + x;
 				int ny = hdy[dir] + y;
-				if (nx < 0 || ny < 0 || nx >= w || ny >= h) continue;
+				if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
 				if (dist[nx][ny][k + 1] >= 0) continue; // already visited
 				if (arr[nx][ny] == 1) continue; // if it is obstacle
-				if (nx == w - 1 && ny == h - 1) {
-					cout << dist[x][y][k] + 1 << '\n';
+				if (nx == h - 1 && ny == w - 1) {
+					cout << dist[x][y][k] + 1<< '\n';
 					return 0;
 				}
 				q.push({ nx,ny,k + 1 });
@@ -63,10 +62,10 @@ int main() {
 		for (int dir = 0; dir < 4; dir++) {
 			int nx = dx[dir] + x;
 			int ny = dy[dir] + y;
-			if (nx < 0 || ny < 0 || nx >= w || ny >= h) continue;
+			if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
 			if (dist[nx][ny][k] >= 0) continue;
 			if (arr[nx][ny] == 1) continue;
-			if (nx == w - 1 && ny == h - 1) {
+			if (nx == h - 1 && ny == w - 1) {
 				cout << dist[x][y][k] + 1 << '\n';
 				return 0;
 			}
@@ -75,6 +74,7 @@ int main() {
 		}
 		
 	}
+	
 	if (w == h && h == 1)
 		cout << 0 << '\n';
 	else cout << -1 << '\n';
