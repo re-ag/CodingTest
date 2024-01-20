da0661dacf08d0adcb967cec991de607c08c6647
Jaeeun Kim <763k357@naver.com>
2024-01-20 16:34:47 +0900

    [BOJ] N-Queens / 골드4 / 연습
    성공~!

--- 0x0B/BOJ_9663.cpp
+++ 0x0B/BOJ_9663.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 /*
 	BOJ 9663 N-Queen
-	
-	https://www.acmicpc.net/problem/9663
-	
-	현재 N-Queens 가 가능한 좌표의 수열을 구하고 있음 
-    ex ( 0,2) (1,0) (2,3) (3,2) 라고 했을 때 이 4가지 좌표의 배열 순서에 따른 경우의 수를 다 체크함
-    아까 N과 M 문제와 똑같이 구현했기 때문에 그럼
 
-    다시 구현 필요
-    0행의 0부터 N-1까지의 열에 대해서 한번만 순회해보기!
+	https://www.acmicpc.net/problem/9663
+
+	현재 N-Queens 가 가능한 좌표의 수열을 구하고 있음
+	ex ( 0,2) (1,0) (2,3) (3,2) 라고 했을 때 이 4가지 좌표의 배열 순서에 따른 경우의 수를 다 체크함
+	아까 N과 M 문제와 똑같이 구현했기 때문에 그럼
+
+	다시 구현 필요
+	0행의 0부터 N-1까지의 열에 대해서 한번만 순회해보기!
 
 */
 #define X first
@@ -27,23 +27,16 @@ bool check(int x, int y) {
 	}
 	return true;
 }
-void func(int k) { // 체스를 k개 놓은 상태
+void func(int k) { // k번째 row에 말을 배치할 거임
 	if (k == n) {
 		++ans;
-		for (auto q : v) {
-			cout << "(" << q.X << ", " << q.Y << ")\n";
-		}
-		//v.clear();
-		cout << " ----------------\n";
 		return;
 	}
-	for(int i = 0; i < n; i++){
-		for (int j = 0; j < n; j++) {
-			if (check(i, j)) {
-				v.push_back({ i,j });
-				func(k + 1);
-				v.pop_back();
-			}
+	for (int i = 0; i < n; i++) {
+		if (check(k, i)) {
+			v.push_back({ k,i });
+			func(k + 1);
+			v.pop_back();
 		}
 	}
 }
