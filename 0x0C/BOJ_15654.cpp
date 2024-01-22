5cbb63a82b961e8a281956985e2da0b67a087891
re-ag <763k357@naver.com>
2024-01-22 14:37:27 +0900

    [BOJ] N과 M(5) / 실버3 / 재풀이
    * isused 의 크기를 N의 범위로 할 필요 없음
    * 그냥 index를 대상으로 isused를 체크하면 됨!

--- 0x0C/BOJ_15654.cpp
+++ 0x0C/BOJ_15654.cpp
@@ -7,21 +7,21 @@ using namespace std;
 int n, m;
 int arr[9];
 int ans[9];
-bool isused[10001];
+bool isused[9];
 // N과 M(1) 문제와 로직 같음
 void func(int k){
   if(k == m){
     for(int i=0; i< m; i++)
-      cout << ans[i] <<' ';
+      cout << arr[ans[i]] <<' ';
     cout << '\n';
     return;
   }
   for(int i =0; i<n; i++){
-    if(!isused[arr[i]]){
-      isused[arr[i]] = true; // arr[i] 사용 처리
-      ans[k] = arr[i]; // ans 에 추가
+    if(!isused[i]){
+      isused[i] = true; // i인덱스 사용 처리(굳이 arr[i] 로 할 필요 없음)
+      ans[k] = i; // ans 에 index i를! 추가
       func(k+1); // 다음 원소를 정하러 한단계 더 들어감
-      isused[arr[i]] = false; // k번째 인덱스를 arr[i] 로 정한 모든 경우에 대해 다 확인했으니, isused = false
+      isused[i] = false; // k번째 인덱스를 arr[i] 로 정한 모든 경우에 대해 다 확인했으니, isused = false
     }
   }
 }
