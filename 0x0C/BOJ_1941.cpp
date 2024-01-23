46bd99e8935c1165a803a92122ea771495d110df
re-ag <763k357@naver.com>
2024-01-23 17:54:21 +0900

    [BOJ] 소문난 칠공주 / 골드3 / 임시

---
+++ 0x0C/BOJ_1941.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+  BOJ 1941 소문난 칠공주
+  https://www.acmicpc.net/problem/1941
+*/
+string arr[5];
+
+void func(int x, int y, int cnt){
+    if(x < 0 || y < 0 || x >= 5 || y>= 5) return;
+    if(cnt == 7){
+
+        return;
+    }
+}
+int main() {
+  for (int i = 0; i < 5; i++)
+    cin >> arr[i];
+  
+}
