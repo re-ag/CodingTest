0e508e9b1712e0d493645ec7709ee4002b9554b0
re-ag <763k357@naver.com>
2024-01-23 20:02:01 +0900

    [BOJ] 소문난 칠공주 / 골드3 / 1시간
    응 아직 못품

--- 0x0C/BOJ_1941.cpp
+++ 0x0C/BOJ_1941.cpp
@@ -5,16 +5,62 @@ using namespace std;
   https://www.acmicpc.net/problem/1941
 */
 string arr[5];
+int ans;
+vector<pair<int, int>> k;
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
 
-void func(int x, int y, int cnt){
-    if(x < 0 || y < 0 || x >= 5 || y>= 5) return;
-    if(cnt == 7){
-
+bool checkK(int x, int y) {
+    for (auto cur : k) {
+        if (cur.first == x && cur.second == y)
+            return false;
+    }
+    return true;
+}
+bool check(int x, int y) {
+    if (x < 0 || y < 0 || x >= 5 || y >= 5)
+        return false;
+    return true;
+}
+void func(int x, int y) {
+    if (!check(x, y))
         return;
+    if (k.size() == 7) {
+        int cnt = 0;
+        for (auto p : k) {
+            if (arr[p.first][p.second] == 'S')
+                cnt++;
+        }
+        if (cnt >= 4)
+            ans++;
+        return;
+    }
+    for (auto p : k) {
+        int i = p.first;
+        int j = p.second;
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = i + dx[dir];
+            int ny = j + dy[dir];
+            if (!check(nx, ny) || !checkK(nx, ny))
+                continue;
+            k.push_back({ nx, ny });
+            func(nx, ny);
+            k.pop_back();
+        }
     }
 }
 int main() {
-  for (int i = 0; i < 5; i++)
-    cin >> arr[i];
-  
+    for (int i = 0; i < 5; i++)
+        cin >> arr[i];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            k.push_back({ i,j });
+            func(i, j);
+            break;
+            k.pop_back();
+        }
+    }
+        
+    cout << ans;
+
 }
